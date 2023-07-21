a73455c6686aa256be572c0c2e3d2c5402e8bc75
8onlichtman <44766460+8onlichtman@users.noreply.github.com>
2023-07-21 11:25:46 +0300

    Update integer_programming_avg.py

--- prtpy/partitioning/integer_programming_avg.py
+++ prtpy/partitioning/integer_programming_avg.py
@@ -16,10 +16,9 @@ import mip
 
 
 def optimal(
-    binner: Binner, numbins: int, items: List[any],
+    binner: Binner, numbins: int, items: List[any], relative_values: List[any] = None,
     copies=1,
     time_limit=inf,
-    weights:List[float]=None,
     verbose=0
 ):
     """
@@ -32,8 +31,8 @@ def optimal(
     :param time_limit: stop the computation after this number of seconds have passed.
 
     >>> from prtpy import BinnerKeepingContents, BinnerKeepingSums
-    >>> optimal(BinnerKeepingSums(), 2, [11.1,11,11,11,22])
-    array([33. , 33.1])
+    >>> optimal(BinnerKeepingSums(), 2, [11.1,11,11,11,22],[0.1,0.9])
+    array([11. , 55.1])
     >>> optimal(BinnerKeepingSums(), 2, [11,11,11,11,22])
     array([33., 33.])
 
@@ -68,23 +67,28 @@ def optimal(
     iitems = range(len(items))
     if isinstance(copies, Number):
         copies = {iitem: copies for iitem in iitems}
-    if weights is None:
-        weights = numbins*[1]
 
     model = mip.Model("partition")
     counts: dict = {
-        iitem: [model.add_var(var_type=mip.INTEGER) for ibin in ibins] 
+        iitem: [model.add_var(var_type=mip.INTEGER) for ibin in ibins]
         for iitem in iitems
     }  # counts[i][j] is a variable that represents how many times item i appears in bin j.
-    bin_sums = [
-        sum([counts[iitem][ibin] * binner.valueof(items[iitem]) for iitem in iitems])/weights[ibin] 
-        for ibin in ibins
-    ]  # bin_sums[j] is a variable-expression that represents the sum of values in bin j.
 
-    z_js = [
-        bin_sums[ibin] - sum(items) / len(ibins)
+    bin_sums = [
+        sum([counts[iitem][ibin] * binner.valueof(items[iitem]) for iitem in iitems])
         for ibin in ibins
     ]
+
+    if relative_values:
+        z_js = [
+            bin_sums[ibin] - sum(items) * relative_values[ibin]
+            for ibin in ibins
+        ]
+    else:
+        z_js = [
+            bin_sums[ibin] - sum(items) / len(ibins)
+            for ibin in ibins
+        ]
 
     t_js = [
         model.add_var(var_type=mip.INTEGER) for ibin in ibins
@@ -126,3 +130,27 @@ if __name__ == "__main__":
     import doctest, logging
     (failures, tests) = doctest.testmod(report=True, optionflags=doctest.FAIL_FAST)
     print("{} failures, {} tests".format(failures, tests))
+
+
+    import pandas as pd
+
+    df = pd.read_csv('tables.csv')
+
+    numbins = 0
+    relative_values = []
+    values = []
+    for i in range (len(df.values)):
+        if (str(df.values[i][11]) != "nan"):
+            numbins = numbins + 1
+            relative_values.append(float((df.values[i][11])[:-1]))
+
+    for i in range(len(df.values)):
+        values.append(int(df.values[i][3].replace(',', '')))
+    import time
+    start = time.time()
+
+    from prtpy import BinnerKeepingContents, BinnerKeepingSums
+    printbins(optimal(BinnerKeepingSums(), numbins, values, relative_values))
+
+    end = time.time()
+    print(end - start)
